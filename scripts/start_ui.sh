9eb645bc7633596e88d54e6d2a636112f3adb0da
Joern Seger <joern.seger@covestro.com>
2021-05-01 13:57:40 +0200

    changed startup logging path and kernel message output

--- scripts/start_ui.sh
+++ scripts/start_ui.sh
@@ -3,6 +3,7 @@
 # only .profile (or .bash_profile or .bash_login) is sourced.
 
 cp /etc/udev/scripts/mount1.sh /etc/udev/scripts/mount.sh
+/bin/echo "3" > /proc/sys/kernel/printk
 setfont /usr/share/consolefonts/Lat15-TerminusBold32x16.psf.gz
 /usr/bin/ui
 bash
