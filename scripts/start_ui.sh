9d42ab8ea00b33948213f654303bebf8542c9538
Joern Seger <joern.seger@covestro.com>
2021-05-02 15:21:54 +0200

    added call for (nfs) mounting script to do in front

--- scripts/start_ui.sh
+++ scripts/start_ui.sh
@@ -4,6 +4,10 @@
 
 cp /etc/udev/scripts/mount1.sh /etc/udev/scripts/mount.sh
 /bin/echo "3" > /proc/sys/kernel/printk
+if [ -f /usr/bin/mount_pre.sh ]
+then
+   /usr/bin/mount_pre.sh
+fi
 setfont /usr/share/consolefonts/Lat15-TerminusBold32x16.psf.gz
 /usr/bin/ui /tmp
 bash
